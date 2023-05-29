06fa9556ac8ab6481052c5773391f6fc8a80c97a
Kasper-Schmidt <94352891+Kasper-Schmidt@users.noreply.github.com>
2023-05-29 18:15:06 +0200

    Add files via upload

--- storefront-child/functions.php
+++ storefront-child/functions.php
@@ -44,3 +44,23 @@ add_filter( 'woocommerce_product_add_to_cart_text', 'woocommerce_custom_product_
 function woocommerce_custom_product_add_to_cart_text() {
     return __( 'Køb', 'woocommerce' );  // Omskriver til "Køb" tekst
 }
+
+
+/**
+ * Hide shipping rates when free shipping is available.
+ * Updated to support WooCommerce 2.6 Shipping Zones.
+ *
+ * @param array $rates Array of rates found for the package.
+ * @return array
+ */
+function my_hide_shipping_when_free_is_available( $rates ) {
+	$free = array();
+	foreach ( $rates as $rate_id => $rate ) {
+		if ( 'free_shipping' === $rate->method_id ) {
+			$free[ $rate_id ] = $rate;
+			break;
+		}
+	}
+	return ! empty( $free ) ? $free : $rates;
+}
+add_filter( 'woocommerce_package_rates', 'my_hide_shipping_when_free_is_available', 100 );
